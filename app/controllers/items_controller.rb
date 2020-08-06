82ac1cdfdf79692d0680c4d898a40d4ee04e8698
saki0508 <sakiogata58@gmail.com>
2020-08-06 21:17:40 +0900

    バリデーション設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,5 +3,26 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
+  end
+
+  def create
+    
+    @item = Item.new(item_params)
+    
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+     
+
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name,:image,:description,:price,:category_id,:state_id,:postage_id,:region_id,:shipping_date_id).merge(user_id: current_user.id)
   end
 end
