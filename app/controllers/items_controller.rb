1fec13874c1558800d731bbfc3bce939f6358344
saki0508 <sakiogata58@gmail.com>
2020-08-10 11:28:30 +0900

    商品情報編集機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
 
-  before_action :set_item, only: [:show]
+  before_action :set_item, only: [:show, :edit]
 
   def index
     @items = Item.all.order("created_at DESC")
@@ -22,6 +22,18 @@ class ItemsController < ApplicationController
   end
 
   def show
+  end
+
+  def edit
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+     redirect_to item_path(@item.id)
+    else
+      render :edit
+    end
   end
 
 
