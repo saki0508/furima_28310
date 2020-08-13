c235e1ad8d0c8bd4fe9255f0b186daac068251e7
saki0508 <sakiogata58@gmail.com>
2020-08-13 11:55:34 +0900

    商品購入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only:[:new,:destroy,:edit]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
+  before_action :sold_out_redirect, only:[:show]
 
   def index
     @items = Item.all.order("created_at DESC")
@@ -41,6 +42,11 @@ class ItemsController < ApplicationController
 
 
   private
+
+  def sold_out_redirect
+    @item = Item.find(params[:id])
+    redirect_to root_path if Address.exists?(item:@item)
+  end
 
   def item_params
     params.require(:item).permit(:name,:image,:description,:price,:category_id,:state_id,:postage_id,:region_id,:shipping_date_id).merge(user_id: current_user.id)
---
+++ app/forms/buyer_address.rb
@@ -0,0 +1,19 @@
+class BuyerAddress
+
+  include ActiveModel::Model
+  attr_accessor :postal_code, :prefecture_id, :city, :block, :building, :phone_number, :user_id, :item_id,:token
+
+
+  validates :postal_code, :prefecture_id, :city, :block, :phone_number, presence: true
+
+  validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/ , message: "Input correctly"}
+  validates :phone_number, format: { with: /\A\d{11}\z/ }
+
+  validates :prefecture_id, numericality: { other_than: 1, message: "Select"} 
+
+  def save
+    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, block: block, building: building, phone_number: phone_number, item_id: item_id)
+    Buyer.create(user_id: user_id, item_id: item_id)
+  end
+
+end
